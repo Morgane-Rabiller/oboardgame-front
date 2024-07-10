29d336682b4abf5e7e885198c69dac34fe5fc963
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-07-10 16:51:30 +0200

    added home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,16 @@ import "./Home.scss";
 
 const Home = () => {
     return (
-        <div>
-            <h1>HOME</h1>
+        <div className="home">
+            <div className="home_description">
+                <p>
+                    <span> -&gt; Crée ta bibliothèque en y ajoutant tes jeux </span><br/>
+                    <span> -&gt; Et c'est parti ! </span><br/>
+                    <span> -&gt; L'application sélectionnera pour toi un jeu</span></p>
+            </div>
+            <div className="home_button-container">
+                <button className="home_button">Je me connecte</button>
+            </div>
         </div>
     );
 };
