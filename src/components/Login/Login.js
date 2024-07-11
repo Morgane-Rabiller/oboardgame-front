2de6e2de1795243801468a3289a79cac97f386dd
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-07-11 13:51:44 +0200

    added connection page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,8 +9,10 @@ const Login = () => {
         <div className="login">
             <h1 className="login_title">Je me connecte</h1>
             <form className="login_form">
-                <InputText type="text" className="p-inputtext-sm" placeholder="Pseudo" />
-                <Password value={value} className="p-inputtext-sm" onChange={(e) => setValue(e.target.value)} toggleMask />
+                <InputText type="text" className="login_form-pseudo p-inputtext-sm" placeholder="Pseudo" />
+                <Password value={value} className="login_form-password p-inputtext-sm" onChange={(e) => setValue(e.target.value)} placeholder="Mot de passe" toggleMask />
+                <button className="login_form-button">Je me connecte</button>
+                <p className="login_form-register">Pas de compte ? <br/> <a href="#" className="login_form-link">Je m'inscris</a></p>
             </form>
         </div>
     );
