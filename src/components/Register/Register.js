ab39b2916be9a621e9f300154b1298a1db8eeacd
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-07-18 14:39:01 +0200

    added email field in register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,6 +11,7 @@ const Register = () => {
             <h1 className="register_title">Je m'inscris</h1>
             <form className="register_form">
                 <InputText type="text" className="register_form-pseudo p-inputtext-sm" placeholder="Pseudo" />
+                <InputText type="email" className="register_form-email p-inputtext-sm" placeholder="Adresse mail" />
                 <Password value={password} className="register_form-password p-inputtext-sm" onChange={(e) => setPassword(e.target.password)} placeholder="Mot de passe" toggleMask />
                 <Password value={passwordRepeat} className="register_form-password p-inputtext-sm" onChange={(e) => setPasswordRepeat(e.target.passwordRepeat)} placeholder="Confirmation du mot de passe" toggleMask />
                 <button className="register_form-button">Créer mon compte</button>
