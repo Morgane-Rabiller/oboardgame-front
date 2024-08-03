0072a6fd9af7829a740470dce53dd34e9de92c92
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-08-03 11:47:15 +0200

    added footer elements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import "./Footer.scss";
+import profile from "../../assets/profile.png";
+import logout from "../../assets/power.png";
 
 const Footer = () => {
     return (
         <footer className="footer">
+            <img src={profile} alt="profile" className="footer_profile" />
+            <p>Jeux de sociétés</p>
+            <img src={logout} alt="logout" className="footer_logout" />
         </footer>
     );
 };
