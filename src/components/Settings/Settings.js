df22a3caeb68374246322f2d1b2797665d4462df
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-09-18 15:18:24 +0200

    added buttons in settings component

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import { InputSwitch } from 'primereact/inputswitch';
+import { Button } from 'primereact/button';
 import "./Settings.scss";
+import { useState } from "react";
 
 const Settings = () => {
+    const [checked, setChecked] = useState(false);
     return (
         <div className="settings">
+            <div className="flex justify-content-between mb-2 ">
+                <p className="hover:underline">Thème</p>
+                <InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} />
+            </div>
+            <Button className="mb-2">Changer mon mot de passe</Button>
+            <Button className="mb-2">Tuto</Button>
         </div>
     );
 };
