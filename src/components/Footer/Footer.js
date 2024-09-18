900da443a5aeff391e057f41bd201fae6c89ef3b
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-09-18 14:58:51 +0200

    added setting icon in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import "./Footer.scss";
-import profile from "../../assets/profile.png";
+import settings from "../../assets/settings.png";
 import power from "../../assets/power.png";
 import { useDispatch } from "react-redux";
 import { logout } from "../../actions/user";
@@ -13,7 +13,7 @@ const Footer = () => {
     }
     return (
         <footer className="footer">
-            <a href="https://google.com"><img src={profile} alt="profile" className="footer_profile" /></a>
+            <a href="https://google.com"><img src={settings} alt="profile" className="footer_setting" /></a>
             <NavLink className={({isActive}) => isActive ? "footer_link footer_link-active" : "footer_link"} to="/jeux-de-societe">Jeux de société</NavLink>
             <button type="button" className="footer_button" onClick={handleClick}> <img src={power} alt="logout" className="footer_logout" /> </button>
         </footer>
