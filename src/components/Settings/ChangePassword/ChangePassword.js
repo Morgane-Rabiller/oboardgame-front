e6454262460dd7e1626b740e2d684d17ba8ca97a
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-09-19 15:27:19 +0200

    added changePassword form

--- src/components/Settings/ChangePassword/ChangePassword.js
+++ src/components/Settings/ChangePassword/ChangePassword.js
@@ -1,10 +1,23 @@
-import "../Settings.scss";
+import { Password } from "primereact/password";
+import "./ChangePassword.scss";
+import { useState } from "react";
+import { Button } from "primereact/button";
 
 const ChangePassword = () => {
-
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
     return (
-        <div className="settings_changePassword">
-        </div>
+        <form className="settings_changePassword mt-4">
+            <label>Ancien de passe</label>
+            <Password value={password} className="mb-2" onChange={(e) => setPassword(e.target.value)} toggleMask />
+            <label>Nouveau mot de passe</label>
+            <Password value={newPassword} className="mb-2" onChange={(e) => setNewPassword(e.target.value)} toggleMask />
+            <label>Confirmation du nouveau mot de passe</label>
+            <Password value={password} className="mb-2" onChange={(e) => setPassword(e.target.value)} toggleMask />
+                <div className="text-center">
+            <Button>Valider</Button>
+            </div>
+        </form>
     );
 };
 
