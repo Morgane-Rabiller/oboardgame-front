e6a4acfbe389c5dbcb12fddfd1d6057bc34e3c48
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-09-25 15:26:31 +0200

    changed place of number players and added types

--- src/components/RandomGame/Randomgame.js
+++ src/components/RandomGame/Randomgame.js
@@ -3,15 +3,12 @@ import React, { useEffect, useState } from "react";
 import { ToggleButton } from 'primereact/togglebutton';
 import { InputNumber } from 'primereact/inputnumber';
 import { Dropdown } from 'primereact/dropdown';
-import card from "../../assets/card.png";
-import dice from "../../assets/dice.png";
-import pawn from "../../assets/pawn.png";
 import { useDispatch, useSelector } from "react-redux";
 import { eraseBoardgameSelected, eraseErrorMessage, selectRandomBoardgame } from "../../actions/library";
 
 const Randomgame = () => {
     const [checked, setChecked] = useState(false);
-    const [players, setPlayers] = useState(1);
+    const [players, setPlayers] = useState(3);
     const [age, setAge] = useState(25);
     const [selectedDuration, setSelectedDuration] = useState(null);
     const userPseudo = useSelector((state) => state.userReducer.pseudo);
@@ -26,15 +23,15 @@ const Randomgame = () => {
     ];
     const [ selectedType, setSelectedType] = useState(null);
     const types = [
-        { name: "---" },
-        { name: <img src={card} alt="cartes" className="img_card" /> },
-        { name: <img src={dice} alt="dés" className="img" /> },
-        { name: <img src={pawn} alt="pions" className="img" /> },
+        { name: "-" },
+        { name: "Cartes" },
+        { name: "Dés" },
+        { name: "Plateau" },
     ];
     const dispatch = useDispatch();
 
     const handleClick = () => {
-        dispatch(selectRandomBoardgame({players, selectedDuration, age, selectedType}));
+        dispatch(selectRandomBoardgame({players, selectedDuration, age, type: selectedType && selectedType.name}));
     };
 
     useEffect(() => {
@@ -52,14 +49,14 @@ const Randomgame = () => {
     return (
         <div className="randomgame">
             <p className="mb-5">Bonjour { userPseudo } 👋</p>
+            <div className="mb-3 flex justify-content-between align-items-baseline">
+                <label htmlFor="minmax-buttons" className="block mb-2">Combien êtes-vous ?</label>
+                <InputNumber inputId="minmax-buttons" size={1} maxLength={2} value={players} onValueChange={(e) => setPlayers(e.value)} mode="decimal" showButtons min={1} max={20} />
+            </div>
             <div>
                 <ToggleButton invalid onIcon="pi pi-eye" offIcon="pi pi-eye-slash" checked={checked} onChange={(e) => setChecked(e.value)} className="toggle-button w-8rem" onLabel="Filtres" offLabel="Filtres"/>
             </div>
             {checked && <div className="randomgame_filters">
-                <div>
-                    <label htmlFor="minmax-buttons" className="block mb-2">Nb joueurs</label>
-                    <InputNumber inputId="minmax-buttons" size={1} maxLength={2} value={players} onValueChange={(e) => setPlayers(e.value)} mode="decimal" showButtons min={1} max={20} />
-                </div>
                 <div>
                     <label htmlFor="minmax-buttons" className="block mb-2">Durée</label>
                     <Dropdown value={selectedDuration} onChange={(e) => setSelectedDuration(e.value)} options={durations} optionLabel="name" 
@@ -80,8 +77,8 @@ const Randomgame = () => {
                 <button className="randomgame_button" onClick={() => handleClick()}>Lancer la sélection aléatoire</button>
             </div>
             <div>
-                {boardgameSelected && <p className="mt-8 font-bold border-2 border-purple-500 p-3 text-center">{boardgameSelected}</p>}
-                {errorMessage && <p className="mt-8 font-bold border-2 border-red-500 p-3 text-center">{errorMessage}</p>}
+                {boardgameSelected && <p className="mt-7 font-bold border-2 border-purple-500 p-3 text-center">{boardgameSelected}</p>}
+                {errorMessage && <p className="mt-7 font-bold border-2 border-red-500 p-3 text-center">{errorMessage}</p>}
             </div>
         </div>
     );
