bde186221aa8a15f0f9966a7064570e2e00ab25f
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-10-02 11:34:48 +0200

    changed sentence in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,12 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home_description">
-                <p>
-                    <span> -&gt; Crée ta bibliothèque en y ajoutant tes jeux </span><br/>
+                <p className="home_p">
+                    <span> -&gt; Tu es en soirée jeu de société avec tes amis et vous ne savez pas quel jeu choisir ? </span><br/>
+                    <span> -&gt; Crée ta bibliothèque en y ajoutant tes jeux à partir de la liste des jeux de société</span><br/>
                     <span> -&gt; Et c'est parti ! </span><br/>
-                    <span> -&gt; L'application sélectionnera pour toi un jeu</span></p>
+                    <span> -&gt; L'application sélectionnera pour toi un jeu</span>
+                </p>
             </div>
             <div className="home_button-container">
                 <button className="home_button" onClick={() => navigate("/connexion")}>Je me connecte</button>
