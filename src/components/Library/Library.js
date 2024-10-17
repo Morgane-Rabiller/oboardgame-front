5ddbebdd7a5b88c30d1cd4e511ce7dd1c7cbf879
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-10-17 15:06:47 +0200

    added choice time option in boardgame add and library update

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -181,10 +181,17 @@ const Library = () => {
                                             </select> : data.type_game}
                                             age={isEditing ? <input type="number" className="w-12" min="2" value={editedData.age} onChange={(e) => handleChange('age', e.target.value)} /> : data.age}
                                             time={isEditing ? <select className="w-13"  value={editedData.time} onChange={(e) => handleChange('time', e.target.value)} >
+                                                <option value="15" key="1">5</option>
+                                                <option value="15" key="1">10</option>
                                                 <option value="15" key="1">15</option>
-                                                <option value="20" key="2">20</option>
-                                                <option value="30" key="3">30</option>
+                                                <option value="15" key="1">20</option>
+                                                <option value="15" key="1">25</option>
+                                                <option value="20" key="2">30</option>
+                                                <option value="30" key="3">35</option>
+                                                <option value="45" key="4">40</option>
                                                 <option value="45" key="4">45</option>
+                                                <option value="45" key="4">50</option>
+                                                <option value="45" key="4">55</option>
                                                 <option value="60" key="5">60</option>
                                             </select> : data.time}
                                         />
